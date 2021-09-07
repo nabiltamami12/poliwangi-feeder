d5b3dcbe5c29885877dc5149aa697b27de3ed111
Dicky <dicky@inagata.com>
2021-09-07 12:02:28 +0700

    Implementasi SPI mandiri

--- resources/views/keuangan/detailSPI.blade.php
+++ resources/views/keuangan/detailSPI.blade.php
@@ -22,11 +22,11 @@
           <form class="form-inline form_detailSPI">
             <div class="form-group col-md-6 p-0">
               <label for="nim" class="mr-3">NIM</label>
-              <input type="text" class="form-control flex-grow-1 mr-md-3" id="nim" value="362055401002" disabled>
+              <input type="text" class="form-control flex-grow-1 mr-md-3" id="nim" value="{{$id}}" disabled>
             </div>
             <div class="form-group col-md-6 p-0 mt-3 mt-md-0">
               <label for="nama" class="mr-3">Nama</label>
-              <input type="text" class="form-control flex-grow-1" id="nama" value="Afkarina ferin verigia" disabled>
+              <input type="text" class="form-control flex-grow-1" id="nama" value="{{$nama}}" disabled>
             </div>
           </form>
 
@@ -38,7 +38,7 @@
           <hr class="my-4">
 
           <div class="table-responsive table_detailSPI">
-            <table class="table align-items-center table-flush table-borderless table-hover">
+            <table class="table align-items-center table-flush table-borderless table-hover" id="datatable">
               <thead class="table-header">
                 <tr>
                   <th scope="col">Tanggal</th>
@@ -48,7 +48,7 @@
               </thead>
 
               <tbody class="table-body">
-                <tr>
+              <tr>
                   <td>13 Juli 2021</td>
                   <td class="col_cicilan text-right">2.000.000</td>
                   <td class="text-capitalize">Cicilan Bulan Juli 2021</td>
@@ -63,6 +63,7 @@
                   <td class="col_cicilan text-right">500.000</td>
                   <td class="text-capitalize">Cicilan Bulan September 2021</td>
                 </tr>
+
               </tbody>
             </table>
           </div>
@@ -70,5 +71,52 @@
       </div>
     </div>
   </div>
+<script>
+const toCurrency = (number) => 
+      Intl.NumberFormat("id-ID", { style : 'currency', currency:'IDR', minimumFractionDigits: 0 }).format(number);
+
+$(document).ready(function() {
+
+var id = "{{$id}}";
+console.log(id);
+dt_url = `${url_api}/keuangan/spi/`+id;
+console.log(dt_url);
+dt_opt = {
+"columnDefs": [
+    {
+      "aTargets": [0],
+      "mData": null,
+      "className": 'text-center pr-0',
+      "mRender": function(data, type, full) {
+        var date = new Date(data['tanggal_pembayaran']).toLocaleDateString();
+        res = date;
+        return res;
+      }
+    },{
+      "aTargets": [1],
+      "mData": null,
+      "className": 'text-right',
+      "mRender": function(data, type, full) {
+        res = toCurrency(data['pembayaran']);
+        return res;
+      }
+    },{
+      "aTargets": [2],
+      "mData": null,
+      "className": 'text-capitalize',
+      "mRender": function(data, type, full) {
+        res = data['keterangan'];
+        return res;
+      }
+    },
+  ], 
+  paging: false,
+  ordering: false,
+  searching: false,
+  info: false
+}
+} 
+);
+</script>
 </section>
 @endsection
