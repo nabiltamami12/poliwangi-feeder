415ba85d4773de8d553a0c5472fa0064a1d53dcb
Denny <denny@inagata.com>
2021-09-22 15:21:17 +0700

    BniEnc and BNIHelper

--- app/Helpers/BniHelper.php
+++ app/Helpers/BniHelper.php
@@ -1,22 +1,35 @@
 <?php
+
 namespace App\Helpers;
+
+use App\Providers\BniEnc;
+use Exception;
 
 /**
  * Library Access BNI API_V3.0.3
  */
 class BniHelper {
-
 	function __construct($action = null, $data = null)
 	{
-		if (!$action) die('aksi tidak ditemukan');
+		if (!$action) throw new Exception('Error: Aksi tidak boleh kosong. contoh: "new BNI($action, $data=null)"');
 		$production = env('APP_ENV') === 'production';
-		$this->url = $production ? env('BNI_URL') : 'https://apibeta.bni-ecollection.com/';
+		self::$url = $production ? env('BNI_URL') : 'https://apibeta.bni-ecollection.com/';
 		$this->data = $data;
+
+		switch ($action) {
+			case 'create':
+				return $this->create('coba');
+				break;
+			
+			default:
+				throw new Exception('Error: Aksi "'.$action.'" tidak ditemukan dalam class BNI.');
+				break;
+		}
 	}
 	
-	public function test()
+	public static function create()
 	{
-		return $this->url;
+		return self::$url;
 	}
 
 }
