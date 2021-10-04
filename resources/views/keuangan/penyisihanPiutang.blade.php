4232083e29e7914f0a8ecc069c861d92847b50a4
Achmad An'im <animfahmy@yahoo.co.id>
2021-10-04 13:51:05 +0700

    penyisihan piutang, ganti style datatable

--- resources/views/keuangan/penyisihanPiutang.blade.php
+++ resources/views/keuangan/penyisihanPiutang.blade.php
@@ -3,7 +3,11 @@
 @section('content')
 <!-- Header -->
 <header class="header"></header>
-
+<style type="text/css">
+  .page-content .table-responsive .table-body td {
+    line-height: 0;
+}
+</style>
 <!-- Page content -->
 <section class="page-content page-content__keuangan container-fluid">
   <div class="row">
@@ -80,36 +84,13 @@
           </form>
         </div>
 
-        <div class="row align-items-center px-3 my-4">
-          <div class="col-12 col-md-6">
-            <form class="form-inline">
-              <div class="form-group row">
-                <select class="form-control form-control-sm" id="dataperhalaman">
-                  <option>10</option>
-                  <option>20</option>
-                  <option>30</option>
-                </select>
-                <label for="dataperhalaman" class="ml-3 mt-2 mt-sm-0">Data per Halaman</label>
-              </div>
-            </form>
-          </div>
-          <div class="col-12 col-md-4 offset-md-2 offset-0 text-right p-0 mt-3 mt-md-0">
-            <form class="search_form" action="">
-              <input class="form-control form-control-sm" type="search" placeholder="Pencarian...">
-              <button type="submit">
-                <i class="iconify-inline" data-icon="bx:bx-search"></i>
-              </button>
-            </form>
-          </div>
-        </div>
-
         <div class="table-responsive">
-          <table class="table align-items-center table-flush table-borderless">
+          <table class="table align-items-center table-flush table-borderless" id="datatable">
             <thead class="table-header">
               <tr>
                 <th scope="col" style="width: 25%">Nama</th>
-                <th colspan="2" scope="col" class="text-center pl-1">kualitas piutang</th>
-                <th colspan="2" scope="col" class="text-center pl-1">Penyisihan piutang</th>
+                <th scope="col" class="text-center pl-1">kualitas piutang</th>
+                <th scope="col" class="text-center pl-1">Penyisihan piutang</th>
                 <th scope="col" class="text-center">Jumlah netto</th>
                 <th scope="col" class="text-center">aksi</th>
               </tr>
@@ -117,100 +98,224 @@
 
             <tbody class="table-body">
               <tr>
-                <td rowspan="5" class="font-weight-bold text-capitalize">Sindy Eka Putri Septiani</td>
-                <td class="bordered pl-1">Lancar</td>
-                <td class="text-right font-weight-bold bordered_sm">Rp. 2.000.000</td>
-                <td class="bordered pl-1">0.5%</td>
-                <td class="text-right font-weight-bold bordered_sm">Rp. 10.000</td>
-                <td rowspan="5" class="text-center font-weight-bold">Rp. 1.990.000</td>
-                <td rowspan="5" class="text-center">
+                <td class="font-weight-bold text-capitalize">Sindy Eka Putri Septiani</td>
+                <td>
+                  <table>
+                    <tr>
+                      <td class="bordered pl-1">Lancar</td>
+                      <td class="text-right font-weight-bold bordered_sm">Rp. 2.000.000</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">Kurang Lancar</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">Diragukan</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">Macet</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr class="bordered">
+                      <td class="pl-1">Jumlah</td>
+                      <td class="text-right font-weight-bold">Rp. 2.000.000</td>
+                    </tr>
+                  </table>
+                </td>
+                <td>
+                  <table>
+                    <tr>
+                      <td class="bordered pl-1">0.5%</td>
+                      <td class="text-right font-weight-bold bordered_sm">Rp. 10.000</td>
+                      
+                      
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">10%</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">50%</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">100%</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr class="bordered">
+                      <td class="pl-1">Jumlah</td>
+                      <td class="text-right font-weight-bold">Rp. 10.000</td>
+                    </tr>
+                  </table>
+                </td>
+                <td class="text-center font-weight-bold">Rp. 1.990.000</td>
+                <td class="text-center">
                   <i class="iconify edit-icon mr-2" data-icon="bx:bx-edit-alt"></i>
                   <i class="iconify delete-icon" data-icon="bx:bx-trash"></i>
                 </td>
               </tr>
               <tr>
-                <td class="bordered pl-1">Kurang Lancar</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-                <td class="bordered pl-1">10%</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-              </tr>
-              <tr>
-                <td class="bordered pl-1">Diragukan</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-                <td class="bordered pl-1">50%</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-              </tr>
-              <tr>
-                <td class="bordered pl-1">Macet</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-                <td class="bordered pl-1">100%</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-              </tr>
-              <tr class="bordered">
-                <td class="pl-1">Jumlah</td>
-                <td class="text-right font-weight-bold">Rp. 2.000.000</td>
-                <td class="pl-1">Jumlah</td>
-                <td class="text-right font-weight-bold">Rp. 10.000</td>
-              </tr>
-
-              <tr>
-                <td rowspan="5" class="font-weight-bold text-capitalize">Sindy Eka Putri Septiani</td>
-                <td class="bordered pl-1">Lancar</td>
-                <td class="text-right font-weight-bold bordered_sm">Rp. 2.000.000</td>
-                <td class="bordered pl-1">0.5%</td>
-                <td class="text-right font-weight-bold bordered_sm">Rp. 10.000</td>
-                <td rowspan="5" class="text-center font-weight-bold">Rp. 1.990.000</td>
-                <td rowspan="5" class="text-center">
+                <td class="font-weight-bold text-capitalize">Sindy Eka Putri Septiani</td>
+                <td>
+                  <table>
+                    <tr>
+                      <td class="bordered pl-1">Lancar</td>
+                      <td class="text-right font-weight-bold bordered_sm">Rp. 2.000.000</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">Kurang Lancar</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">Diragukan</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">Macet</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr class="bordered">
+                      <td class="pl-1">Jumlah</td>
+                      <td class="text-right font-weight-bold">Rp. 2.000.000</td>
+                    </tr>
+                  </table>
+                </td>
+                <td>
+                  <table>
+                    <tr>
+                      <td class="bordered pl-1">0.5%</td>
+                      <td class="text-right font-weight-bold bordered_sm">Rp. 10.000</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">10%</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">50%</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">100%</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr class="bordered">
+                      <td class="pl-1">Jumlah</td>
+                      <td class="text-right font-weight-bold">Rp. 10.000</td>
+                    </tr>
+                  </table>
+                </td>
+                <td class="text-center font-weight-bold">Rp. 1.990.000</td>
+                <td class="text-center">
                   <i class="iconify edit-icon mr-2" data-icon="bx:bx-edit-alt"></i>
                   <i class="iconify delete-icon" data-icon="bx:bx-trash"></i>
                 </td>
-              </tr>
-              <tr>
-                <td class="bordered pl-1">Kurang Lancar</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-                <td class="bordered pl-1">10%</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-              </tr>
-              <tr>
-                <td class="bordered pl-1">Diragukan</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-                <td class="bordered pl-1">50%</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-              </tr>
-              <tr>
-                <td class="bordered pl-1">Macet</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-                <td class="bordered pl-1">100%</td>
-                <td class="text-right font-weight-bold bordered_sm">-</td>
-              </tr>
-              <tr>
-                <td class="pl-1">Jumlah</td>
-                <td class="text-right font-weight-bold">Rp. 2.000.000</td>
-                <td class="pl-1">Jumlah</td>
-                <td class="text-right font-weight-bold">Rp. 10.000</td>
               </tr>
             </tbody>
           </table>
-        </div>
-
-        <div class="row justify-content-between align-items-center table-information">
-          <h3>Menampilkan 1 sampai 2 dari 2 total data</h3>
-          <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item disabled" aria-label="Previous">
-                <a class="page-link" href="#" tabindex="-1">Previous</a>
-              </li>
-              <li class="page-item active">
-                <a class="page-link" href="#">1<span class="sr-only">(current)</span></a>
-              </li>
-              <li class="page-item disabled" aria-label="Next">
-                <a class="page-link" href="#">Next</a>
-              </li>
-            </ul>
-          </nav>
         </div>
       </div>
     </div>
   </div>
 </section>
+<script>
+$(document).ready(function() {
+
+var nomor = 1;
+dt_url = `${url_api}/keuangan/rekapitulasi/penyisihanpiutang`;
+dt_opt = {
+"columnDefs": [
+    {
+      "aTargets": [0],
+      "mData": null,
+      "className": 'text-center pr-0',
+      "mRender": function(data, type, full) {
+        return data['nama'];
+      }
+    },{
+      "aTargets": [1],
+      "mData": null,
+      "bSortable": false,
+      "bSearchable": false,
+      "className": '',
+      "mRender": function(data, type, full) {
+        return `<table>
+                    <tr>
+                      <td class="bordered pl-1">Lancar</td>
+                      <td class="text-right font-weight-bold bordered_sm">Rp. 2.000.000</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">Kurang Lancar</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">Diragukan</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">Macet</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr class="bordered">
+                      <td class="pl-1">Jumlah</td>
+                      <td class="text-right font-weight-bold">Rp. 2.000.000</td>
+                    </tr>
+                  </table>`;
+      }
+    },{
+      "aTargets": [2],
+      "mData": null,
+      "bSortable": false,
+      "bSearchable": false,
+      "className": '',
+      "mRender": function(data, type, full) {
+        return `<table>
+                    <tr>
+                      <td class="bordered pl-1">0.5%</td>
+                      <td class="text-right font-weight-bold bordered_sm">Rp. 10.000</td>
+                      
+                      
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">10%</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">50%</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr>
+                      <td class="bordered pl-1">100%</td>
+                      <td class="text-right font-weight-bold bordered_sm">-</td>
+                    </tr>
+                    <tr class="bordered">
+                      <td class="pl-1">Jumlah</td>
+                      <td class="text-right font-weight-bold">Rp. 10.000</td>
+                    </tr>
+                  </table>`;
+      }
+    },{
+      "aTargets": [3],
+      "mData": null,
+      "className": 'text-center font-weight-bold',
+      "mRender": function(data, type, full) {
+        return formatAngka(data['total']);
+      }
+    },{
+      "aTargets": [4],
+      "mData": null,
+      "bSortable": false,
+      "bSearchable": false,
+      "className": 'text-center',
+      "mRender": function(data, type, full) {
+        return `<i class="iconify edit-icon mr-2" data-icon="bx:bx-edit-alt"></i>
+                  <i class="iconify delete-icon" data-icon="bx:bx-trash"></i>`;
+      }
+    }
+  ]}
+
+}
+ 
+);
+</script>
 @endsection
