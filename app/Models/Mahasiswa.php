2313d8693eea0f43f30290ddabb1ee1b8ccddd19
dennysw <dennysetiawisnugraha@gmail.com>
2021-10-04 09:25:34 +0700

    import keuangan v.beta

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -10,6 +10,7 @@ class Mahasiswa extends Model
     use HasFactory;
     public $timestamps = false;
     protected $table = 'mahasiswa';
+    protected $primaryKey = 'nomor';
 
     protected $fillable = [
         'nama',
