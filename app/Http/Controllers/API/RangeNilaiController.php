3142f57102cbbb6ce9c0a3cf857f0473b175a796
Denny <denny@inagata.com>
2021-10-19 15:18:49 +0700

    fixing range nilai

--- app/Http/Controllers/API/RangeNilaiController.php
+++ app/Http/Controllers/API/RangeNilaiController.php
@@ -8,6 +8,7 @@ use App\Models\RangeNilai;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\QueryException;
 use DB;
+use Illuminate\Support\Carbon;
 
 class RangeNilaiController extends Controller
 {
@@ -66,6 +67,9 @@ class RangeNilaiController extends Controller
 
         try {
             foreach ($request->data as $key => $value) {
+                $value['tanggal_awal'] = Carbon::parse($value['tanggal_awal'])->format('Y-m-d');
+                $value['tanggal_akhir'] = Carbon::parse($value['tanggal_akhir'])->format('Y-m-d');
+                // echo json_encode($value);
                 $data = RangeNilai::create($value);
             }
             $this->data = $data;
