21793db489f2aaf447bd635ff0ada8ee7d3a1db3
Denny <denny@inagata.com>
2021-11-01 11:30:21 +0700

    menambah keterangan ketika diterima di poltek lain

--- resources/views/mahasiswaBaru/dashboardMaba.blade.php
+++ resources/views/mahasiswaBaru/dashboardMaba.blade.php
@@ -25,7 +25,7 @@
       </div>
     </div>
     <div class="col-md-6">
-      <div class="card shadow padding--medium card_presensi mt-0 mt-md-4">
+      <div id="card_prodi_pilihan" class="card shadow padding--medium card_presensi mt-0 mt-md-4">
         <div class="card-header p-0">
           <div class="row align-items-center">
             <div class="col">
@@ -42,7 +42,29 @@
 
           </div>
         </div>
-          <button type="button" onclick="func_verifikasi()" class="btn btn-success w-100 mt-4 rounded-sm btn-no-jadwal">Verifikasi Data</button>
+          <button type="button" id="btn_verifikasi" onclick="func_verifikasi()" class="btn btn-success w-100 mt-4 rounded-sm btn-no-jadwal">Verifikasi Data</button>
+      </div>
+      <div id="card_prodi_detail" hidden class="card shadow padding--medium card_presensi mt-0 mt-md-4">
+        <div class="card-header p-0">
+          <div class="row align-items-center">
+            <div class="col">
+              <h3 class="mb-0">Detail Prodi</h3>
+            </div>
+          </div>
+          <hr class="my-4">
+        </div>
+        <div id="card_presensi" class="card-body p-0">
+          <p id="text_poltek_luar" hidden class="">Silahkan lanjutkan untuk mendaftar ulang pada politeknik yang diterima</p>
+          <p id="text_detail" class="text-status">Sedang Dalam Proses</p>
+          <div id="detail_penerimaan">
+            <h6 class="mb-0 mb-2">Prodi Pilihan</h6>
+            <h5 class="mb-0 mb-3" id="prodi_detail"></h5>
+            <h6 class="mb-0 mb-2">Biaya SPI</h6>
+            <h5 class="mb-0 mb-3" id="spi_detail"></h5>
+            <h6 class="mb-0 mb-2">Biaya UKT</h6>
+            <h5 class="mb-0 mb-3" id="ukt_detail"></h5>
+          </div>
+        </div>
       </div>
     </div>
     <div class="col-md-6">
@@ -56,7 +78,7 @@
           <hr class="my-4">
         </div>
         <div id="card_presensi" class="card-body p-0">
-          <p class="text-status">Sedang Dalam Proses</p>
+          <p id="text_penerimaan" class="text-status">Sedang Dalam Proses</p>
           <div id="info_penerimaan" hidden>
             <h6 class="mb-0 mb-2">Diterima Di</h6>
             <h5 class="mb-0 mb-3" id="poltek_pilihan"></h5>
@@ -114,13 +136,44 @@ function getDashboard() {
           $('#list_poltek').append(html);
         }
         if (res.data.info.status!=null) {
-          if (res.data.info.status=="T") {
+          var info = res.data.info;
+          if (info.status=="T") {
             $('#info_penerimaan').attr('hidden',false);
             $('.text-status').attr('hidden',true);
             $('#poltek_pilihan').text('-')
             $('#prodi_pilihan').text('-')
             $('#status_pilihan').text('Tidak Lolos')
             $('#status_pilihan').addClass('text-danger')
+            $('#btn_verifikasi').attr('disabled',true)
+          }else if (info.status=="Y") {
+            $('#card_prodi_pilihan').attr('hidden',true) 
+            $('#card_prodi_detail').attr('hidden',false)
+
+            if (info.ukt_kelompok==null) {
+              if (info.program_studi_luar!=null) {
+                $('#text_detail').attr('hidden',true);
+                $('#text_poltek_luar').attr('hidden',false);
+                $('#detail_penerimaan').attr('hidden',true)
+              }
+              if (info.program_studi!=null) {
+                $('#text_detail').attr('hidden',false)
+                $('#detail_penerimaan').attr('hidden',true)
+              }
+            }else{
+              $('#prodi_detail').html(info.prodi)
+              $('#spi_detail').html(formatAngka(info.spi))
+              $('#ukt_detail').html(formatAngka(info.ukt))
+              $('#text_detail').attr('hidden',true)
+              $('#detail_penerimaan').attr('hidden',false)
+            }
+             
+            $('#info_penerimaan').attr('hidden',false);
+            $('#text_penerimaan').attr('hidden',true);
+            $('#poltek_pilihan').text(info.politeknik)
+            $('#prodi_pilihan').text(info.prodi)
+            $('#status_pilihan').text('Lolos')
+            $('#status_pilihan').addClass('text-success')
+
           }
         }
       }
