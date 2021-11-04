cbe8fc65f17049a55f6a18bd97a12bc9940ea3bc
dennysw <dennysetiawisnugraha@gmail.com>
2021-11-04 09:35:42 +0700

    master mahasiswa datatable serverside

---
+++ app/Models/Datatables/MahasiswaMasterDatatable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Datatables;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Lib\Datatable;
+use DB;
+
+class MahasiswaMasterDatatable extends Datatable
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $table = 'mahasiswa';
+    protected $primaryKey = 'nomor';
+
+    // datatable
+    protected $table_datatables = 'mahasiswa as m';
+    protected $select = array('m.nomor','m.nrp','m.nama','DATE_FORMAT(m.tgllahir, "%Y-%m-%d") as tgllahir','m.notelp','m.email');
+    protected $column_order = array('m.nomor','m.nrp','m.nama', 'm.tgllahir','m.notelp','m.email', null);
+    protected $column_search = array('m.nrp','m.nama','m.tgllahir','m.notelp','m.email');
+    protected $order = array('m.nomor' => 'desc');
+
+    function __construct($where = []){
+        $this->where = $where;
+        parent::__construct();
+    }
+    public function extra_datatables()
+    {
+        $this->dt->where($this->where);
+
+        $this->dtc->where($this->where);
+    }
+    // end datatable
+}
--- resources/views/akademik/masterData/datamahasiswa.blade.php
+++ resources/views/akademik/masterData/datamahasiswa.blade.php
@@ -101,62 +101,18 @@
 		var nomor = 1;
 		dt_url = `${url_api}/mahasiswa?program_studi=${$('#program_studi').val()}&kelas=${$('#kelas').val()}`;
 		dt_opt = {
-			"columnDefs": [
-			{
-				"aTargets": [0],
-				"mData": null,
-				"mRender": function(data, type, full) {
-					res = nomor++;
-					return (res==null)?"-":res;
-				}
-			},{
-				"aTargets": [1],
-				"mData": null,
-				"mRender": function(data, type, full) {
-					res = data['nrp'];
-					return (res==null)?"-":res;
-				}
-			},{
-				"aTargets": [2],
-				"mData": null,
-				"mRender": function(data, type, full) {
-					res = data['nama'];
-					return (res==null)?"-":res;
-				}
-			},{
-				"aTargets": [3],
-				"mData": null,
-				"mRender": function(data, type, full) {
-					res = data['tgllahir'];
-					return (res==null)?"-":res;
-				}
-			},{
-				"aTargets": [4],
-				"mData": null,
-				"mRender": function(data, type, full) {
-					res = data['notelp'];
-					return (res==null)?"-":res;
-				}
-			},{
-				"aTargets": [5],
-				"mData": null,
-				"mRender": function(data, type, full) {
-					res = data['email'];
-					return (res==null)?"-":res;
-				}
-			},{
-				"aTargets": [6],
-				"mData": null,
-				"mRender": function(data, type, full) {
-					var id = data['nomor'];
-					var text_hapus = data['nama'];
-					var btn_update = `<span class="iconify edit-icon text-primary" onclick='update_btn(${id})' data-icon="bx:bx-edit-alt" ></span>` 
-					var btn_delete = `<span class="iconify delete-icon text-primary" data-icon="bx:bx-trash"  onclick='delete_btn(${id},"mahasiswa","mahasiswa","${text_hapus}")'></span>`; 
-					res = btn_update+" "+btn_delete;
-					return res;
-				}
-			},
-			]}
+			serverSide: true,
+			// 	"aTargets": [6],
+			// 	"mData": null,
+			// 	"mRender": function(data, type, full) {
+			// 		var id = data['nomor'];
+			// 		var text_hapus = data['nama'];
+			// 		var btn_update = `<span class="iconify edit-icon text-primary" onclick='update_btn(${id})' data-icon="bx:bx-edit-alt" ></span>` 
+			// 		var btn_delete = `<span class="iconify delete-icon text-primary" data-icon="bx:bx-trash"  onclick='delete_btn(${id},"mahasiswa","mahasiswa","${text_hapus}")'></span>`
+			// 		res = btn_update+" "+btn_delete;
+			// 		return res;
+			// 	}
 		}
+	}
 	</script>
 	@endsection
