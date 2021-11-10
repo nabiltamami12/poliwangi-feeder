0bba88ab2dd948e7d3f5166a4e550e7ac1d405f4
Denny <denny@inagata.com>
2021-11-10 11:22:59 +0700

    fixing tampilan dashboard

--- resources/views/mahasiswaBaru/dashboardMaba.blade.php
+++ resources/views/mahasiswaBaru/dashboardMaba.blade.php
@@ -24,7 +24,7 @@
         </div>
       </div>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-8">
       <div id="card_prodi_pilihan" class="card shadow padding--medium card_presensi mt-0 mt-md-4">
         <div class="card-header p-0">
           <div class="row align-items-center">
@@ -42,32 +42,13 @@
 
           </div>
         </div>
-          <button type="button" id="btn_verifikasi" onclick="func_verifikasi()" class="btn btn-success w-100 mt-4 rounded-sm btn-no-jadwal">Verifikasi Data</button>
-      </div>
-      <div id="card_prodi_detail" hidden class="card shadow padding--medium card_presensi mt-0 mt-md-4">
-        <div class="card-header p-0">
-          <div class="row align-items-center">
-            <div class="col">
-              <h3 class="mb-0">Detail Prodi</h3>
-            </div>
-          </div>
-          <hr class="my-4">
-        </div>
-        <div id="card_presensi" class="card-body p-0">
+          <button type="button" id="btn_poltek_lain" class="btn w-100 mt-4 rounded-sm btn-info_transparent text-primary">*Segera lakukan verifikasi data pada Politeknik bersangkutan</button>
+          <button type="button" id="btn_verifikasi" onclick="func_verifikasi()" hidden class="btn btn-success w-100 mt-4 rounded-sm btn-no-jadwal">Verifikasi Data</button>
+          <button type="button" id="btn_berkas" onclick="func_berkas()" hidden class="btn btn-success w-100 mt-4 rounded-sm btn-no-jadwal">Upload Berkas</button>
           <p id="text_poltek_luar" hidden class="">Silahkan lanjutkan untuk mendaftar ulang pada politeknik yang diterima</p>
-          <p id="text_detail" class="text-status">Sedang Dalam Proses</p>
-          <div id="detail_penerimaan">
-            <h6 class="mb-0 mb-2">Prodi Pilihan</h6>
-            <h5 class="mb-0 mb-3" id="prodi_detail"></h5>
-            <h6 class="mb-0 mb-2">Biaya SPI</h6>
-            <h5 class="mb-0 mb-3" id="spi_detail"></h5>
-            <h6 class="mb-0 mb-2">Biaya UKT</h6>
-            <h5 class="mb-0 mb-3" id="ukt_detail"></h5>
-          </div>
-        </div>
       </div>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-4">
       <div class="card shadow padding--medium card_presensi mt-0 mt-md-4">
         <div class="card-header p-0">
           <div class="row align-items-center">
@@ -78,14 +59,17 @@
           <hr class="my-4">
         </div>
         <div id="card_presensi" class="card-body p-0">
-          <p id="text_penerimaan" class="text-status">Sedang Dalam Proses</p>
-          <div id="info_penerimaan" hidden>
+          <div id="info_penerimaan">
             <h6 class="mb-0 mb-2">Diterima Di</h6>
-            <h5 class="mb-0 mb-3" id="poltek_pilihan"></h5>
+            <h5 class="mb-0 mb-3" id="poltek_pilihan">-</h5>
             <h6 class="mb-0 mb-2">Pada Program Studi</h6>
-            <h5 class="mb-0 mb-3" id="prodi_pilihan"></h5>
+            <h5 class="mb-0 mb-3" id="prodi_pilihan">-</h5>
+            <h6 class="mb-0 mb-2">Biaya SPI</h6>
+            <h5 class="mb-0 mb-3" id="spi_pilihan">-</h5>
+            <h6 class="mb-0 mb-2">Biaya UKT</h6>
+            <h5 class="mb-0 mb-3" id="ukt_pilihan">-</h5>
             <h6 class="mb-0 mb-2">Status</h6>
-            <h5 class="mb-0 mb-3" id="status_pilihan"></h5>
+            <h5 class="mb-0 mb-3" id="status_pilihan"><span>Belum ada pengumuman</span></h5>
           </div>
         </div>
       </div>
@@ -108,6 +92,9 @@ getDashboard()
 
 function func_verifikasi() {
   window.location.href = "{{url('/mahasiswabaru/verifikasidata')}}"
+}
+function func_verifikasi() {
+  window.location.href = "{{url('/mahasiswabaru/daftarulang')}}"
 }
 
 function getDashboard() {
@@ -136,6 +123,9 @@ function getDashboard() {
           $('#list_poltek').append(html);
         }
         if (res.data.info.status!=null) {
+          $('#btn_poltek_lain').attr('hidden',true);
+          $('#btn_verifikasi').attr('hidden',true);
+          $('#btn_berkas').attr('hidden',false);
           var info = res.data.info;
           if (info.status=="T") {
             $('#info_penerimaan').attr('hidden',false);
@@ -144,37 +134,33 @@ function getDashboard() {
             $('#prodi_pilihan').text('-')
             $('#status_pilihan').text('Tidak Lolos')
             $('#status_pilihan').addClass('text-danger')
-            $('#btn_verifikasi').attr('disabled',true)
-          }else if (info.status=="Y") {
-            $('#card_prodi_pilihan').attr('hidden',true) 
-            $('#card_prodi_detail').attr('hidden',false)
 
-            if (info.ukt_kelompok==null) {
-              if (info.program_studi_luar!=null) {
-                $('#text_detail').attr('hidden',true);
-                $('#text_poltek_luar').attr('hidden',false);
-                $('#detail_penerimaan').attr('hidden',true)
-              }
-              if (info.program_studi!=null) {
-                $('#text_detail').attr('hidden',false)
-                $('#detail_penerimaan').attr('hidden',true)
-              }
-            }else{
-              $('#prodi_detail').html(info.prodi)
-              $('#spi_detail').html(formatAngka(info.spi))
-              $('#ukt_detail').html(formatAngka(info.ukt))
-              $('#text_detail').attr('hidden',true)
-              $('#detail_penerimaan').attr('hidden',false)
+            $('#btn_poltek_lain').attr('hidden',true);
+            $('#btn_verifikasi').attr('hidden',true);
+            $('#btn_berkas').attr('hidden',true);
+  
+          }else if (info.status=="Y") {
+
+            if (info.ukt_kelompok!=null) {
+              $('#prodi_pilihan').html(info.prodi)
+              $('#spi_pilihan').html(formatAngka(info.spi))
+              $('#ukt_pilihan').html(formatAngka(info.ukt)) 
+            }
+            if (info.program_studi_luar!=null) {
+              $('#btn_verifikasi').attr('hidden',true);
+              $('#btn_berkas').attr('hidden',true);
+              $('#btn_poltek_lain').attr('hidden',false);
             }
              
-            $('#info_penerimaan').attr('hidden',false);
-            $('#text_penerimaan').attr('hidden',true);
             $('#poltek_pilihan').text(info.politeknik)
             $('#prodi_pilihan').text(info.prodi)
             $('#status_pilihan').text('Lolos')
             $('#status_pilihan').addClass('text-success')
-
           }
+        }else{
+          $('#btn_verifikasi').attr('hidden',false);
+          $('#btn_berkas').attr('hidden',true);
+          $('#btn_poltek_lain').attr('hidden',true);
         }
       }
     }
